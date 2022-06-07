fac8275762ba7fe31d8b4f4bd05fe89e8dea35b1
hiropon-kun <gudorobetta@yahoo.co.jp>
2022-06-07 11:55:16 +0900

    event show

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,7 +7,7 @@ use App\Http\Requests\UpdateEventRequest;
 use App\Models\Event;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB; 
-use App\Sevices\EventService;
+use App\Services\EventService;
 
 class EventController extends Controller
 {
@@ -70,15 +70,15 @@ class EventController extends Controller
         return to_route('events.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
     public function show(Event $event)
     {
-        //
+        $event = Event::findOrFail($event->id);
+        $eventDate = $event->eventDate;
+        $startTime = $event->startTime;
+        $endTime = $event->endTime;
+
+        // dd($eventDate, $startTime, $endTime);
+        return view('manager.events.show',compact('event','eventDate','startTime','endTime'));
     }
 
     /**
--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Sevices;
+namespace App\Services;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB; 
